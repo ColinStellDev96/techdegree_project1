5363afbf0deda6ad2d9e787e5118986368ef51fe
Colin Stell <ColinStellDev96@users.noreply.github.com>
2018-02-06 21:21:04 -0700

    quote array of objects

--- js/script.js
+++ js/script.js
@@ -2,3 +2,35 @@
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+var quotes = [
+    {
+        quote: "I intend to smoke a good cigar to the glory of God before I go to bed tonight.",
+        source: "Charles Spurgeon",
+        citation: "Metropolitan Tabernacle",
+        year: 1874
+    },
+    {
+        quote: "Always, always make the best beer possible.",
+        source: "Brian Dunn",
+        citation: "Great Divide Brewing",
+        year: 2017
+    },
+    {
+        quote: "It’s times like these you learn to live again.",
+        source: "Foo Fighers",
+        citation: "One by One",
+        year: 2002
+    },
+    {
+        quote: "Sometimes the greatest way to say something is to say nothing at all",
+        source: "Justin Timberlake",
+        citation: "Man of the Woods",
+        year: 2018
+    },
+    {
+        quote: "You outshined the best there was, Rewrote who I could be, When I held you for the first time I knew I had to survive",
+        source: "Converge",
+        citation: "The Dusk in Us",
+        year: 2017
+    },
+]
