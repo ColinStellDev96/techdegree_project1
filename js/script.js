c61d1035539095b588abdc1f1c3e5f2dfe11577e
Colin Stell <ColinStellDev96@users.noreply.github.com>
2018-02-13 20:40:27 -0700

    added random background color and timed quote change

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
-
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 //array of objects containing quote, source, citation, and year
@@ -8,14 +7,12 @@ var quotes = [
     {
         quote: "I intend to smoke a good cigar to the glory of God before I go to bed tonight.",
         source: "Charles Spurgeon",
-        citation: '',
         year: 1874
     },
     {
         quote: "Always, always make the best beer possible.",
         source: "Brian Dunn",
         citation: "Great Divide Brewing",
-        year: ''
     },
     {
         quote: "It’s times like these you learn to live again.",
@@ -36,26 +33,49 @@ var quotes = [
         year: 2017
     },
 ];
-// console.log(quotes);
+
+var colors = ['#fc053a', '#147ad6', '#f77c18', '#d80896', '#bb8b49'];
+
+var repeatQuotes = [];
+var repeatColors = [];
 
 // function that takes an array, and selects a random object from that array.
 function getRandomQuote () {
-    //variable that creates a random number using the quotes arra
-    randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
-    //returns the random quote selected
-    return randomQuote;
+    randomQuote = quotes[Math.floor(Math.random() * quotes.length)]; //variable that creates a random number using the quotes arra
+    quoteSplice = quotes.splice(randomQuote, 1)[0]; //splice randomQuote object, 1 is the index value the splice stops at, [0] indicates access of the first item in the array
+    repeatQuotes.push(quoteSplice); //pushing each random quote into the global repeatQuotes array which starts as an empty array, but populates as the getRandomQuote function is called.
+    if (quotes.length === 0) {
+        quotes = repeatQuotes;
+        repeatQuotes = []; // this logic allows us to start over again once all the quotes have been viewed
+    }
+    return quoteSplice;
 };
-console.log(getRandomQuote());
+// console.log(getRandomQuote());
+
+function getRandomColor () {
+    randomColor = colors[Math.floor(Math.random() * colors.length)];
+    colorSplice = colors.splice(randomColor, 1)[0];
+    repeatColors.push(colorSplice);
+    if (colors.length === 0) {
+        colors = repeatColors;
+        repeatColors = [];
+    }
+    return colorSplice;
+};
 
 function printQuote () {
     //calls getRandomQuote function and assigns selected quote to new variable
     var printText = getRandomQuote();
+    var newColor = getRandomColor();
     // varialbe that prints object values in HTML based on class.
     var newQuotePrint = '<p class="quote">' + printText.quote + '</p>' + '<p class="source">' + printText.source  + '<span class="citation">' + printText.citation + '</span>' + '<span class="year">' + printText.year + '</span>'+'</p>';
-    if (printText.citation === '') {
+    if (printText.citation === undefined) {
         newQuotePrint = '<p class="quote">' + printText.quote + '</p>' + '<p class="source">' + printText.source + '<span class="year">' + printText.year + '</span>'+'</p>';
-    } if (printText.year === '') {
+    } if (printText.year === undefined) {
         newQuotePrint = '<p class="quote">' + printText.quote + '</p>' + '<p class="source">' + printText.source + '<span class="citation">' + printText.citation + '</span>'+'</p>';
     }
     document.getElementById('quote-box').innerHTML = newQuotePrint;
+    document.body.style.backgroundColor = newColor;
 };
+
+window.setInterval(printQuote, 30000);
